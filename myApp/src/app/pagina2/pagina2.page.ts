3876be474355bbd751c9c946941d74e533970901
MatiasLealTapia <matiasa.lealt.23@gmail.com>
2022-08-22 15:11:32 -0400

    commit

---
+++ myApp/src/app/pagina2/pagina2.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+@Component({
+  selector: 'app-pagina2',
+  templateUrl: './pagina2.page.html',
+  styleUrls: ['./pagina2.page.scss'],
+})
+export class Pagina2Page implements OnInit {
+  dato:any;
+  constructor(private activeroute: ActivatedRoute, private router: Router) {
+    this.activeroute.queryParams.subscribe(params => {
+      if (this.router.getCurrentNavigation().extras.state) {
+        this.dato = this.router.getCurrentNavigation().extras.state.user;
+        console.log(this.dato)
+      }
+});
+}
+
+
+  ngOnInit() {
+  }
+
+}
